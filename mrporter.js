494c069be40c8ac9c6bb3e3e7bdf24a036fd1535
cxllum <32437860+cx-xyz@users.noreply.github.com>
2018-03-19 21:55:51 +0000

    Create mrporter.js

---
+++ mrporter.js
@@ -0,0 +1,25 @@
+javascript:
+
+var Region = prompt("Please enter your region: ", "Mr Porter Region Changer");
+
+if (Region) {
+    post('https://www.mrporter.com/changecountry.mrp', {
+        language: 'en',
+        selCountry: Region + ',am,gbp,true',
+        confirmSelection: 'Confirm Selection'
+    });
+}
+
+function post(a, b) {
+    const c = (e, f) => Object.assign(document.createElement(e), f),
+        d = c('form', {
+            action: a,
+            method: 'post',
+            style: 'display: none'
+        });
+    for (const [e, f] of Object.entries(b)) d.appendChild(c('input', {
+        name: e,
+        value: f
+    }));
+    document.body.appendChild(d), d.submit()
+}
